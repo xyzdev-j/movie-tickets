3cb01a083152878576d5e55ec0189c8a005ea78e
xyzdev-j <alirezaxyz8@gmail.com>
2025-05-02 12:26:23 +0330

    Add 3 movies

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args)
     {
         //Just a start point for now.
+        Movie braveHeart = new Movie("Braveheart", 8.3f, "Action, War", "Monday");
+        Movie forrestGump = new Movie("Forrest Gump", 8.8f, "Drama, Romance", "Saturday");
+        Movie twelveAngryMen = new Movie("12 Angry Men", 9.0f, "Drama, Crime", "Friday");
     }
 }
