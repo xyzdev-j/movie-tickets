be7d52b3df36a7cc0547e3132d1b4d9764ecda2d
xyzdev-j <alirezaxyz8@gmail.com>
2025-05-02 12:30:13 +0330

    Add available tickets for each movie

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,5 +5,9 @@ public class Main {
         Movie braveHeart = new Movie("Braveheart", 8.3f, "Action, War", "Monday");
         Movie forrestGump = new Movie("Forrest Gump", 8.8f, "Drama, Romance", "Saturday");
         Movie twelveAngryMen = new Movie("12 Angry Men", 9.0f, "Drama, Crime", "Friday");
+
+        Ticket braveHeartTickets = new Ticket("Braveheart", 1000);
+        Ticket forrestGumpTickets = new Ticket("Forrest Gump", 1000);
+        Ticket twelveAngryMenTickets = new Ticket("12 Angry Men", 1000);
     }
 }
